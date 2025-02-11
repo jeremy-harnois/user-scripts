8d2b33e94860948d4efd93e79c1a9df18748a4f6
Jeremy Harnois <jeremy-harnois@users.noreply.github.com>
2025-02-11 12:39:59 -0500

    added proton-calendar.user.js

---
+++ proton-calendar.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @version         1.0.0
+// @name            Proton Calendar
+// @author          Jeremy Harnois
+// @match           https://calendar.proton.me/u/*
+// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.0.0/proton-calendar.lib.js
+// @supportURL      https://github.com/jeremy-harnois/user-scripts/issues
+// @namespace       https://github.com/jeremy-harnois/user-scripts
+// ==/UserScript==
+
+/* global doNotDimPastEventsIn */
+
+//doNotDimPastEventsIn(/*['Calendar']*/);
