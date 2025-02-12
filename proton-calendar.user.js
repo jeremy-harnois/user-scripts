6042c45fd3ad11c78a6d1d300ff1a1935426509c
Jeremy Harnois <jeremy-harnois@users.noreply.github.com>
2025-02-12 17:36:09 -0500

    feat(proton-calendar): add `advanceOnMouseWheel()` (#2)

--- proton-calendar.user.js
+++ proton-calendar.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @version         1.0.0
+// @version         1.1.0
 // @name            Proton Calendar
 // @author          Jeremy Harnois
 // @match           https://calendar.proton.me/u/*
-// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.0.0/proton-calendar.lib.js
+// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.1.0/proton-calendar.lib.js
 // @supportURL      https://github.com/jeremy-harnois/user-scripts/issues
 // @namespace       https://github.com/jeremy-harnois/user-scripts
 // ==/UserScript==
 
-/* global doNotDimPastEventsIn */
+/* global advanceOnMouseWheel, doNotDimPastEventsIn */
 
+//advanceOnMouseWheel();
 //doNotDimPastEventsIn(/*['Calendar']*/);
