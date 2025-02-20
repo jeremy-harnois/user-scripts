11a724178028e6857852eaefbdfcb8b795ba8d8b
Jeremy Harnois <jeremy-harnois@users.noreply.github.com>
2025-02-20 18:21:20 -0500

    release(proton-calendar): bump version

--- proton-calendar.user.js
+++ proton-calendar.user.js
@@ -3,8 +3,8 @@
 // @name            Proton Calendar
 // @author          Jeremy Harnois
 // @match           https://calendar.proton.me/u/*
-// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.Y.Z/proton.lib.js
-// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.1.0/proton-calendar.lib.js
+// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.2.0/proton.lib.js
+// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.2.0/proton-calendar.lib.js
 // @supportURL      https://github.com/jeremy-harnois/user-scripts/issues
 // @namespace       https://github.com/jeremy-harnois/user-scripts
 // ==/UserScript==
