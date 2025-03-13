628d27fd18e5eb095b076468db6f40114f506b4d
Jeremy Harnois <jeremy-harnois@users.noreply.github.com>
2025-03-13 19:12:28 -0400

    fix(proton-calendar): scripts not always applied

--- proton-calendar.user.js
+++ proton-calendar.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @version         1.2.0
+// @version         1.2.1
 // @name            Proton Calendar
 // @author          Jeremy Harnois
 // @match           https://calendar.proton.me/u/*
-// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.2.0/proton.lib.js
-// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.2.0/proton-calendar.lib.js
+// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.2.1/proton.lib.js
+// @require         https://raw.githubusercontent.com/jeremy-harnois/user-scripts/refs/tags/1.2.1/proton-calendar.lib.js
 // @supportURL      https://github.com/jeremy-harnois/user-scripts/issues
 // @namespace       https://github.com/jeremy-harnois/user-scripts
+// @run-at          document-end
 // ==/UserScript==
 
 /* global advanceOnMouseWheel, doNotDimPastEventsIn, goToTodayOnMouseClick, waitUntilAppLoads */
